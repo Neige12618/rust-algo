e70bc7db99471fb162c924b9e9271b22a961ead7
Neige <1574417117@qq.com>
2024-05-22 21:51:33 +0800

    加入文件重复判断，防止覆盖已有代码

--- bin/src/gen.rs
+++ bin/src/gen.rs
@@ -1,17 +1,17 @@
-use std::env;
-
 use tokio::io::AsyncWriteExt;
 
 use crate::model::daily_info::QuestionInfo;
 
 pub async fn create_template_file(daily_info: QuestionInfo) {
-    println!("{:?}", env::current_dir());
-    let mut file = tokio::fs::File::create(&format!(
-        "./solution/solution{}.rs",
-        daily_info.base_info.id
-    ))
-    .await
-    .unwrap();
+    let path_str = format!("./solution/solution{}.rs", daily_info.base_info.id);
+    let file_path = std::path::Path::new(&path_str);
+
+    if file_path.exists() {
+        println!("file already exists");
+        return;
+    }
+
+    let mut file = tokio::fs::File::create(file_path).await.unwrap();
 
     file.write_all(daily_info.to_string().as_bytes())
         .await
