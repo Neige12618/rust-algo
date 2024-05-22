68d8b0b5435d1f2d202da31c018ce877f9b9ab2b
Neige <1574417117@qq.com>
2024-05-22 22:00:26 +0800

    将新加入的代码自动加入lib.rs

--- bin/src/gen.rs
+++ bin/src/gen.rs
@@ -3,7 +3,8 @@ use tokio::io::AsyncWriteExt;
 use crate::model::daily_info::QuestionInfo;
 
 pub async fn create_template_file(daily_info: QuestionInfo) {
-    let path_str = format!("./solution/solution{}.rs", daily_info.base_info.id);
+    let question_id = daily_info.base_info.id;
+    let path_str = format!("./solution/solution{}.rs", question_id);
     let file_path = std::path::Path::new(&path_str);
 
     if file_path.exists() {
@@ -16,6 +17,19 @@ pub async fn create_template_file(daily_info: QuestionInfo) {
     file.write_all(daily_info.to_string().as_bytes())
         .await
         .unwrap();
+
+    let lib_path = std::path::Path::new("./solution/lib.rs");
+    let mut lib_file = tokio::fs::File::options()
+        .append(true)
+        .open(lib_path)
+        .await
+        .unwrap();
+
+    lib_file
+        .write(format!("pub mod solution{};", question_id).as_bytes())
+        .await
+        .unwrap();
+    lib_file.flush().await.unwrap();
 }
 
 #[cfg(test)]
