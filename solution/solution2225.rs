d386508658df7b78841a28ca5ec9e7c66eea3bc2
Neige <1574417117@qq.com>
2024-05-22 14:18:56 +0800

    加入题目描述

--- solution/solution2225.rs
+++ solution/solution2225.rs
@@ -1,3 +1,18 @@
+/// 2225.找出输掉零场或一场比赛的玩家
+/// 中等
+///
+/// 给你一个整数数组 matches 其中 matches[i] = [winneri, loseri] 表示在一场比赛中 winneri 击败了 loseri 。
+///
+/// 返回一个长度为 2 的列表 answer ：
+/// answer[0] 是所有 没有 输掉任何比赛的玩家列表。
+/// answer[1] 是所有恰好输掉 一场 比赛的玩家列表。
+/// 两个列表中的值都应该按 递增 顺序返回。
+///
+/// 注意：
+/// 只考虑那些参与 至少一场 比赛的玩家。
+/// 生成的测试用例保证 不存在 两场比赛结果 相同 。
+///
+/// https://leetcode.cn/problems/find-players-with-zero-or-one-losses/description/
 pub struct Solution;
 
 use std::collections::HashMap;
